38b0cc9c27607614e6c83b146167ce3ef4c4a64c
and1lima <47044853+and1lima@users.noreply.github.com>
2019-05-28 21:14:46 -0300

    Continuação navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,16 +11,11 @@ import { MaterialModule } from '../app/material/material.module';
 import { TodoComponent } from './todo/todo.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-<<<<<<< HEAD
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { MatSidenavModule } from '@angular/material';
-=======
 import { InicioComponent } from './inicio/inicio.component';
 import { LeitorQrCodeComponent } from './leitor-qr-code/leitor-qr-code.component';
 
 import { ZXingScannerModule } from '@zxing/ngx-scanner';
 import { GeoLocComponent } from './geo-loc/geo-loc.component';
->>>>>>> 62ba3d2832a16de111e4c7571242ea2795010337
 
 @NgModule({
   declarations: [
@@ -38,14 +33,8 @@ import { GeoLocComponent } from './geo-loc/geo-loc.component';
     ReactiveFormsModule,
     BrowserAnimationsModule,
     MaterialModule,
-<<<<<<< HEAD
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    MatSidenavModule
-  
-=======
     ZXingScannerModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
->>>>>>> 62ba3d2832a16de111e4c7571242ea2795010337
   ],
   providers: [],
   bootstrap: [AppComponent]
