84eb3476aae726212011f033abd4d9f29b7bfa30
Rodrigo W. Mendes <r.wmendes@hotmail.com>
2019-05-31 21:45:33 -0300

    Início item 11.

--- src/app/services/evento.service.ts
+++ src/app/services/evento.service.ts
@@ -8,6 +8,41 @@ export class Evento {
   providedIn: 'root'
 })
 export class EventoService {
-
+  mock: [
+      {
+        'eventCode': '1aa3kjhnjhsafj8yi99', 
+        'eventName': 'Python', 
+        'oficineCode': 1, 
+        'oficineName': 'Python', 
+        'oficineLocal': '',
+        'instructorName': '',
+        'startDateTime': '',
+        'endDateTime': '',
+        'beforeInterval': '',
+        'afterInterval': '',
+      },
+      {
+        'eventCode': 'jkhsdnfasuh78747435', 
+        'eventName': 'Redes', 
+        'oficineCode': 2, 
+        'oficineName': 'Redes', 
+        'instructorName': '',
+        'startDateTime': '',
+        'endDateTime': '',
+        'beforeInterval': '',
+        'afterInterval': '',
+      },
+      {
+        'eventCode': 'kljashf878583475kkh', 
+        'eventName':, 'AWS', 
+        'oficineCode': 3, 'AWS', 
+        'oficineName': 'AWS', 
+        'instructorName': '',
+        'startDateTime': '',
+        'endDateTime': '',
+        'beforeInterval': '',
+        'afterInterval': '',
+      }
+    ]
   constructor() { }
 }
